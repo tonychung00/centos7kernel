6cf955bbd7cc0d7206e5f39ff50e6edf0464183e
tonychung00 <tonychung00@gmail.com>
2014-08-07 13:58:53 -0700

    initial kernel-3.10.0-123

---
+++ linux-3.10/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#0 SMP Sun Jul 27 09:10:38 PDT 2014"
+#define LINUX_COMPILE_BY "tc"
+#define LINUX_COMPILE_HOST "localhost.localdomain"
+#define LINUX_COMPILER "gcc version 4.8.2 20140120 (Red Hat 4.8.2-16) (GCC) "
